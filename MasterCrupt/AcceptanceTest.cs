7b0c501e8fcce192407325b8c6ad0543c2322c09
Stéphane Bagnier <stephane.bagnier@gmail.com>
2014-10-12 18:04:39 +0200

    renaming, stronger gitignore and gitattribute and NUnit instead of MSTest

---
+++ MasterCrupt/AcceptanceTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace MasterCrupt
+{
+    [TestFixture]
+    public class AcceptanceTest
+    {
+        [Test]
+        public void TestSecret()
+        {
+            UI ui = new UI();
+            Assert.That(ui.EncryptMessage("Secret"), Is.EqualTo("Leeted: S3cr3t"));
+        }
+    }
+}
--- MasterCrupt/Properties/AssemblyInfo.cs
+++ MasterCrupt/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MasterCrüpt")]
+[assembly: AssemblyTitle("MikadoMethodKata")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Agical AB")]
-[assembly: AssemblyProduct("MasterCrüpt")]
-[assembly: AssemblyCopyright("Copyright © Agical AB 2010")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MikadoMethodKata")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("05ab9a99-404a-423b-a4e4-bb2634b70a17")]
+[assembly: Guid("891fc055-0ae3-4bea-a585-68b1fd734cd1")]
 
 // Version information for an assembly consists of the following four values:
 //
